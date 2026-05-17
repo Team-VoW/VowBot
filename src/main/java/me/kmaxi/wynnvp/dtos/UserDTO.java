80a832fecab97a1d6cea87cda9fe60e61185ab44
kmaxi <66730396+kmaxii@users.noreply.github.com>
2026-05-17 20:23:18 +0200

    feat: use new discord integration api (#33)

--- src/main/java/me/kmaxi/wynnvp/dtos/UserDTO.java
+++ src/main/java/me/kmaxi/wynnvp/dtos/UserDTO.java
@@ -1,6 +1,6 @@
 package me.kmaxi.wynnvp.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,84 +10,21 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
-    private int id;
+    private int userId;
     private String displayName;
-    private String email;
-    private boolean publicEmail;
-    private String avatarLink;
-    private String bio;
-    private String lore;
-    private boolean systemAdmin;
     private long discordId;
     private String discordName;
-    private String youtube;
-    private String twitter;
-    private String castingCallClub;
-    private List<RoleDTO> roles;
+    private String avatarUrl;
+    private PictureType avatarType;
+    private List<String> roleNames;
 
-    @Getter
-    @Setter
-    @ToString
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class RoleDTO {
-        private String name;
-        private String color;
-        private int weight;
-
-        public RoleDTO() {
-            // Default constructor needed for Jackson
-        }
-
-        public RoleDTO(String roleName) {
-            this.name = roleName;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-            RoleDTO roleDTO = (RoleDTO) obj;
-            return name != null && name.equals(roleDTO.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return name != null ? name.hashCode() : 0;
-        }
-    }
-
-    public String getFullPostArguments() {
-        return "discordName=" + discordName + "&" +
-                "discordId=" + discordId + "&" +
-                "roles=" + getRolesArguments() + "&" +
-                "imgurl=" + avatarLink;
-    }
-
-    public String getChangingArguments() {
-        return "discordName=" + discordName + "&" +
-                "discordId=" + discordId + "&" +
-                "roles=" + getRolesArguments();
-    }
-
-    private String getRolesArguments() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[");
-
-        //For each role user has in discord
-        roles.forEach(role -> {
-            String roleName = role.getName();
-
-            stringBuilder.append("\"").append(roleName).append("\",");
-        });
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        stringBuilder.append("]");
-
-        return stringBuilder.toString();
+    public enum PictureType {
+        @JsonProperty("Default")
+        DEFAULT,
+        @JsonProperty("Discord")
+        DISCORD,
+        @JsonProperty("Manual")
+        MANUAL
     }
 }
